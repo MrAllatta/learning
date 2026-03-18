8449e9124aa2f7747f5ebd2ab6510b3654789bc1
MrAllatta <ericallatta@gmail.com>
2026-03-18 11:42:46 -0400

    vibe coded query utility for injecting sql

--- utils/query.py
+++ utils/query.py
@@ -1,8 +1,11 @@
+"""query.py."""
+
 from pathlib import Path
 from google.cloud import bigquery
 
 CLIENT = bigquery.Client()
 SQL_DIR = Path(__file__).resolve().parent.parent / "sql"
+
 
 def load_sql(name: str, **params) -> str:
     """Load a .sql file and optionally format with parameters."""
@@ -11,6 +14,7 @@ def load_sql(name: str, **params) -> str:
     if params:
         sql = sql.format(**params)
     return sql
+
 
 def run(name: str, **params):
     """Load SQL file, execute on BigQuery, return DataFrame."""
